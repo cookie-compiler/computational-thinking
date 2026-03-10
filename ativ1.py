3a517e3a4f0712f98744f461695071281b469983
cookie-compiler <days.by.art@gmail.com>
2026-03-10 01:30:47 +0000

    ativ1

---
+++ ativ1.py
@@ -0,0 +1,12 @@
+print("Beah", end="\n")
+
+a = float(input("Digite um numero: "))
+b = float(input("Digite outro numero: "))
+calculo = (2*a) * (3*b)
+print(f"O resultado do calculo é {calculo:.2f}", end="\n")
+
+a = float(input("Digite um numero: "))
+b = float(input("Digite outro numero: "))
+c = float(input("Digite outro numero: "))
+soma = a + b + c
+print(f"A soma dos numeros escolhidos é {soma:.2f}", end="\n")
--- teste.py
+++ teste.py
@@ -1,10 +1,19 @@
 def hello_world(text):
     print(text)
 
+def soma(a, b):
+    return a + b
+
 hello_world("print")
 
 um = 1.1
 print(str(um)) # float em string =)
 
-num = int(input("Digite um numero: "))
-print(f")
+s = 0
+for i in range(0, 5):
+    num = float(input("Digite um numero: "))
+    print(f"O dobro do numero escolhido é {num*2:.2f}")
+    s = soma(s, num)
+    s += num*2
+
+print(f"A soma dos numeros escolhidos é {s:.2f}")
